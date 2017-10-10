2573fd5d245ec5ee9feac36889a4a004ab6430b0
maddiegold <maddiegold@wifi-roaming-128-4-54-171.host.udel.edu>
2017-10-10 15:31:53 -0400

    PS3

--- PS3/src/main/java/pkgCore/Deck.java
+++ PS3/src/main/java/pkgCore/Deck.java
@@ -1,14 +1,14 @@
 package pkgCore;
-
 import java.util.ArrayList;
 import java.util.Collections;
-
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
+import pkgException.DeckException;
 
-public class Deck {
+public class Deck
+{
 
-	private ArrayList<Card> cardsInDeck;
+	private ArrayList<Card> cardsInDeck = new ArrayList<Card>();
 
 	public Deck() {
 		for (eSuit eSuit : eSuit.values()) {
@@ -19,19 +19,57 @@ public class Deck {
 		Collections.shuffle(cardsInDeck);
 	}
 
-	//TODO: Fix the Draw method so it throws an exception if the deck is empty
-	public Card Draw() {
+	public Card Draw() throws DeckException {
+		if (this.cardsInDeck.size() == 0) {
+			throw new DeckException(this);
+		}
 		return cardsInDeck.remove(0);
 	}
+		
+	public Card DrawSuit(eSuit eSuit) {
+		for (Card c: this.cardsInDeck) {
+			if (c.geteSuit() == eSuit) {
+				cardsInDeck.remove(c); 
+				return c;
+			}
+		}
+		return null;
+	}
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
+	public Card DrawRank(eRank eRank) {
+		for (Card c: this.cardsInDeck) {
+			if (c.geteRank() == eRank) {
+				cardsInDeck.remove(c); 
+				return c;
+			}
+		}
+		return null;
+	}
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
+	public int CountSuit(eSuit eSuit) {
+		int iCardNumber = 0;
+		for (Card c: this.cardsInDeck) {
+			if (c.geteSuit() == eSuit)
+				iCardNumber++;
+		}
+		return iCardNumber;	
+	}
+		
+	public int CountRank(eRank eRank) {
+		int iCardNumber = 0;
+		for (Card c: this.cardsInDeck) {
+			if (c.geteRank() == eRank)
+				iCardNumber++;
+		}
+		return iCardNumber;	
+	}
 
-	//TODO: Write a method that will return the number of a given eSuit left in the deck.
-	
-	//TODO: Write a method that will return the number of a given eRank left in the deck.
-	
-	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
-	
+	public int CountCount(Card c) {
+		for (Card cards: cardsInDeck) {
+			if (cards == c)
+				return 1; 
+		}
+		return 0;
+	}
+
 }
--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -1,32 +1,65 @@
 package pkgCore;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
 
 public class DeckTest {
-
-	@Test
-	public void TestEmptyDeck() {
-		//TODO: Build a deck, draw until you get a DeckException
+	@Test (expected = DeckException.class) 
+	public void TestEmptyDeck() throws DeckException {
+		Deck NewDeck = new Deck();
+		for (int z = 1; z <= 100; z++) 
+			NewDeck.Draw();
 	}
 	
 	@Test
 	public void TestDrawSuit() {
-		//TODO: Build a deck, test the Draw(eSuit) method
+		Deck NewDeck = new Deck();
+		Card NewCard = NewDeck.DrawSuit(eSuit.DIAMONDS);
+
+		assertTrue(NewCard.geteSuit() == eSuit.DIAMONDS);
+		assertTrue(NewCard.geteSuit() != eSuit.DIAMONDS);
+		assertFalse(NewCard.geteSuit() == eSuit.CLUBS);
+		assertNull(NewDeck);
 	}
 	
 	@Test
 	public void TestDrawRank() {
-		//TODO: Build a deck, test the Draw(eRank) method
+		Deck NewDeck = new Deck();
+		Card NewCard = NewDeck.DrawRank(eRank.FOUR);
+		assertTrue(NewCard.geteRank() == eRank.FOUR);
+		assertFalse(NewCard.geteRank() == eRank.FIVE);
 	}
 	
+	@Test
 	public void TestDeckRankCount() {
-		//TODO: Build a deck, test the DeckRankCount method
+		Deck NewDeck = new Deck();
+		assertEquals(NewDeck.CountRank(eRank.SIX), 4);
 	}
 	
+	@Test
 	public void TestDeckSuitCount() {
-		//TODO: Build a deck, test the DeckSuitCount method
+		Deck NewDeck = new Deck();
+		assertEquals(NewDeck.CountSuit(eSuit.SPADES), 13);
+	}
+	
+	@Test
+	public void TestCardCount() {
+		int c = 5;
+		int cards = 5;
+		
+		assertTrue(CountCard(c) == 1);
+		Deck NewDeck = new Deck();
+		
+		
+		
+		
+		Card NewSuit = NewDeck.DrawSuit(eSuit.DIAMONDS);
+		Card NewRank = NewDeck.DrawRank(eRank.FIVE);
+		assertTrue(NewSuit.geteSuit() == eSuit.DIAMONDS);
+		assertTrue(NewRank.geteRank() == eRank.FIVE);
+
 	}
 
 }
